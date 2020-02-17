4dd0deb965bc2c1f9aa4e93b98fae96488ff7004
PabloPintor <pablo@pablopintor.com>
2020-02-17 14:27:08 +0100

    Personaje se mueve, cambiando entre los 4 gifs

---
+++ js/clases.js
@@ -0,0 +1,92 @@
+"user strict";
+class Personaje { // Cualquier personaje que se mueva. Puede ser personaje principal o enemigos.
+    constructor(capa) {
+        this.capa = capa;
+        this.izquierda = capa.offset().left;
+        this.arriba = capa.offset().top;
+        this.altura = capa.outerHeight(true);
+        this.anchura = capa.outerWidth(true);
+        this.derecha = this.izquierda + this.anchura;
+        this.abajo = this.arriba + this.altura;
+    }
+    colisionaPorAbajo(tipo) {
+        let colisiona = false;
+        if (tipo == "terreno") {
+            let terrenos = getTerrenos();
+            terrenos.forEach(val => {
+                if (this.abajo + 10 > val.arriba && this.arriba < val.abajo && this.derecha > val.izquierda && this.izquierda < val.derecha) {
+                    colisiona = true;
+                }
+            });
+        }
+        return colisiona;
+    }
+    colisionaPorArriba(tipo) {
+        let colisiona = false;
+        if (tipo == "terreno") {
+            let terrenos = getTerrenos();
+            terrenos.forEach(val => {
+                if (this.abajo > val.arriba && this.arriba - 10 < val.abajo && this.derecha > val.izquierda && this.izquierda < val.derecha) {
+                    colisiona = true;
+                }
+            });
+        }
+        return colisiona;
+    }
+    colisionaPorDerecha(tipo) {
+        let colisiona = false;
+        if (tipo == "terreno") {
+            let terrenos = getTerrenos();
+            terrenos.forEach(val => {
+                if (this.abajo > val.arriba && this.arriba < val.abajo && this.derecha + 10 > val.izquierda && this.izquierda < val.derecha) {
+                    colisiona = true;
+                }
+            });
+        }
+        return colisiona;
+    }
+    colisionaPorIzquierda(tipo) {
+        let colisiona = false;
+        if (tipo == "terreno") {
+            let terrenos = getTerrenos();
+            terrenos.forEach(val => {
+                if (this.abajo > val.arriba && this.arriba < val.abajo && this.derecha > val.izquierda && this.izquierda - 10 < val.derecha) {
+                    colisiona = true;
+                }
+            });
+        }
+        return colisiona;
+    }
+    moverArriba() {
+        this.capa.animate({ top: this.arriba -= 10 }, { duration: 10, queue: false });
+        this.actualizaCoordenadas();
+    }
+    moverAbajo() {
+        this.capa.animate({ top: this.arriba += 10 }, { duration: 10, queue: false });
+        this.actualizaCoordenadas();
+    }
+    moverDerecha() {
+        this.capa.animate({ left: this.izquierda += 10 }, { duration: 10, queue: false });
+        this.actualizaCoordenadas();
+    }
+    moverIzquierda() {
+        this.capa.animate({ left: this.izquierda -= 10 }, { duration: 10, queue: false });
+        this.actualizaCoordenadas();
+    }
+    actualizaCoordenadas() {
+        this.derecha = this.izquierda + this.anchura;
+        this.abajo = this.arriba + this.altura;
+    }
+}
+
+class Terreno { // Cualquier capa inmovil
+    constructor(capa) {
+        this.capa = capa;
+        this.izquierda = capa.offset().left;
+        this.arriba = capa.offset().top;
+        this.altura = capa.outerHeight(true);
+        this.anchura = capa.outerWidth(true);
+        this.derecha = this.izquierda + this.anchura;
+        this.abajo = this.arriba + this.altura;
+    }
+}
--- js/codigo.js
+++ js/codigo.js
@@ -1,5 +1,62 @@
-$( function() {
-    $("#puntuaciones").dialog();
+var personaje;
+var ultimaDireccion = "";
+
+$(function() {
+    //dialogo puntuaciones
+    //$("#puntuaciones").dialog();
+
+    personaje = new Personaje($('#medico'));
+
+    let keys = {};
+
+    $(document).keydown(function(e) {
+        keys[e.which] = true;
+        mueve();
+    });
+    $(document).keyup(function(e) {
+        delete keys[e.which];
+        mueve();
+    });
+
+    //mover dependiendo de codigo tecla
+    function mueve() {
+        if (keys[37]) {
+            if (!personaje.colisionaPorIzquierda("terreno")) {
+                if (ultimaDireccion != "izquierda"){
+                    $("#medico").attr("src", "img/personajes/correr-izquierda.gif");
+                }
+                personaje.moverIzquierda();
+                ultimaDireccion = "izquierda";
+            }
+        }
+        if (keys[38]) {
+            if (!personaje.colisionaPorArriba("terreno")) {
+                if (ultimaDireccion != "arriba"){
+                    $("#medico").attr("src", "img/personajes/correr-arriba.gif");
+                }
+                personaje.moverArriba();
+                ultimaDireccion = "arriba";
+            }
+        }
+        if (keys[39]) {
+            if (!personaje.colisionaPorDerecha("terreno")) {
+                if (ultimaDireccion != "derecha"){
+                    $("#medico").attr("src", "img/personajes/correr-derecha.gif");
+                }
+                personaje.moverDerecha();
+                ultimaDireccion = "derecha";
+            }
+        }
+        if (keys[40]) {
+            if (!personaje.colisionaPorAbajo("terreno")) {
+                if (ultimaDireccion != "abajo"){
+                    $("#medico").attr("src", "img/personajes/correr-abajo.gif");
+                }
+                personaje.moverAbajo();
+                ultimaDireccion = "abajo";
+            }
+        }
+    }
 
 
 
@@ -9,5 +66,19 @@ $( function() {
 
 
 
-    
+
+
+
+
+
 });
+
+//devuelve array con objetos terreno con cada capa que tenga que colisionar
+function getTerrenos() {
+    let terrenos = [];
+    $(".pared").each((i,val) => {
+        let terreno = new Terreno($(val));
+        terrenos.push(terreno);
+    });
+    return terrenos;
+}
